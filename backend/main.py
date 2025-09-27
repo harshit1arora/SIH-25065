c2d4b97da27842818f0b0b2e02f272158d418077
Harshit Arora <harshitarora1065@gmail.com>
2025-09-28 00:52:00 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -538,4 +538,6 @@ def add_assessment_feedback(assessment_id: int, feedback: schemas.FeedbackSchema
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
